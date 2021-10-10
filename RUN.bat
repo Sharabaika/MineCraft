df8bfd023931017e69693932884d6c57d4d8ff85
VladSharobaicko <61946717+VladSharobaicko@users.noreply.github.com>
2021-10-10 22:12:01 +0300

    move to bukkit
    move to bukkit

--- RUN.bat
+++ RUN.bat
@@ -1,1 +1,1 @@
-java -Xmx1024M -Xms1024M -jar minecraft_server.1.17.1.jar nogui
+java -Xmx1024M -Xms1024M -jar craftbukkit-1.17.1.jar nogui
--- RUN.sh
+++ RUN.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "running"
-java -Xmx1024M -Xms1024M -jar minecraft_server.1.17.1.jar nogui
+java -Xmx1024M -Xms1024M -jar craftbukkit-1.17.1.jar nogui
